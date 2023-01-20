6c46c39853aa4c58ff0e71e38ef92b198599dfea
Justin Bento <bento.a.justin@gmail.com>
2023-01-20 17:55:38 -0500

    moved perks into data field

---
+++ lib/data.tsx
@@ -0,0 +1,28 @@
+const perks = [
+  {
+    name: 'Relestate Data Located In One Place',
+    imageSrc: 'https://tailwindui.com/img/ecommerce/icons/icon-delivery-light.svg',
+    description:
+      "Order now and you'll get delivery absolutely free. Well, it's not actually free, we just price it into the products. Someone's paying for it, and it's not us.",
+  },
+  {
+    name: 'Easily Search',
+    imageSrc: 'https://tailwindui.com/img/ecommerce/icons/icon-warranty-light.svg',
+    description:
+      "We have a 10 year warranty with every product that you purchase, whether thats a new pen or organizer, you can be sure we'll stand behind it.",
+  },
+  {
+    name: 'Exchanges',
+    imageSrc: 'https://tailwindui.com/img/ecommerce/icons/icon-returns-light.svg',
+    description:
+      'We understand that when your product arrives you might not particularly like it, or you ordered the wrong thing. Conditions apply here.',
+  },
+  {
+    name: 'For the planet',
+    imageSrc: 'https://tailwindui.com/img/ecommerce/icons/icon-planet-light.svg',
+    description:
+      "Like you, we love the planet, and so we've pledged 1% of all sales to the preservation and restoration of the natural environment.",
+  },
+]
+
+export {perks }
