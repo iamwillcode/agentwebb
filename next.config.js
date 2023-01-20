c92c0cab8cf3f2b536c60f796f128ee8fac19b95
Justin Bento <bento.a.justin@gmail.com>
2023-01-20 16:47:22 -0500

    included data to reduce headaces

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
+  images: {
+    domains: [  
+      'cdn.sanity.io', 
+      'images.unsplash.com',
+      "tailwindui.com"
+    ],
+  },
 }
 
 module.exports = nextConfig
