ad087d6122dd4d394f27f74a6cf7dc996ef2bae8
Justin Bento <bento.a.justin@gmail.com>
2023-01-20 19:16:55 -0500

    fixed header icons and other stuff

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,9 +12,9 @@ const navigation = [
 
 export default function Header() {
   return (
-    <header className="flex min-h-full flex-col shadow mb-16">
+    <header className="flex flex-col min-h-full shadow">
       <Popover className="relative bg-white">
-        <div className="mx-auto max-w-7xl sm:px-6 lg:px-8 p-4 lg:p-0">
+        <div className="p-4 mx-auto max-w-7xl sm:px-6 lg:px-8 lg:p-0">
           <div className="flex items-center justify-between py-6 md:justify-start md:space-x-10">
             <div className="flex justify-start lg:w-0 lg:flex-1">
               <Link href="/">
@@ -23,9 +23,9 @@ export default function Header() {
               </Link>
             </div>
             <div className="-my-2 -mr-2 md:hidden">
-              <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+              <Popover.Button className="inline-flex items-center justify-center p-2 text-gray-400 bg-white rounded-md hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500">
                 <span className="sr-only">Open menu</span>
-                <HiMenu className="h-6 w-6" aria-hidden="true" />
+                <HiMenu className="w-6 h-6" aria-hidden="true" />
               </Popover.Button>
             </div>
             <nav className="hidden space-x-10 md:flex">
@@ -53,9 +53,9 @@ export default function Header() {
         >
           <Popover.Panel
             focus
-            className="absolute inset-x-0 top-0 origin-top transform p-2 transition md:hidden"
+            className="absolute inset-x-0 top-0 p-2 transition origin-top transform md:hidden"
           >
-            <div className="overflow-hidden rounded-lg bg-white shadow-md ring-1 ring-black ring-opacity-5">
+            <div className="overflow-hidden bg-white rounded-lg shadow-md ring-1 ring-black ring-opacity-5">
               <div className="flex items-center justify-between px-5 pt-4">
                 <div>
                   <Link href="/">
@@ -64,39 +64,23 @@ export default function Header() {
                   </Link>
                 </div>
                 <div className="-mr-2">
-                  <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-600">
+                  <Popover.Button className="inline-flex items-center justify-center p-2 text-gray-400 bg-white rounded-md hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-600">
                     <span className="sr-only">Close menu</span>
-                    <HiX className="h-6 w-6" aria-hidden="true" />
+                    <HiX className="w-6 h-6" aria-hidden="true" />
                   </Popover.Button>
                 </div>
               </div>
               <div className="pt-5 pb-6">
-                <div className="space-y-1 px-2">
+                <div className="px-2 space-y-1">
                   {navigation.map((item) => (
                     <a
                       key={item.name}
                       href={item.href}
-                      className="block rounded-md px-3 py-2 text-base font-medium text-gray-900 hover:bg-gray-50"
+                      className="block px-3 py-2 text-base font-medium text-gray-900 rounded-md hover:bg-gray-50"
                     >
                       {item.name}
                     </a>
                   ))}
-                </div>
-                <div className="mt-6 px-5">
-                  <a
-                    href="#"
-                    className="block w-full rounded-md bg-indigo-600 py-3 px-4 text-center font-medium text-white shadow hover:bg-indigo-700"
-                  >
-                    Sign up
-                  </a>
-                </div>
-                <div className="mt-6 px-5">
-                  <p className="text-center text-base font-medium text-gray-500">
-                    Existing customer?{" "}
-                    <a href="#" className="text-gray-900 hover:underline">
-                      Sign in
-                    </a>
-                  </p>
                 </div>
               </div>
             </div>
