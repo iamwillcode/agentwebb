08098eef39d6b70b0c50ecd0590dae4171fdbccb
Justin Bento <bento.a.justin@gmail.com>
2023-01-21 03:57:20 -0500

    created docs page for agent webb

---
+++ pages/docs.tsx
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+export default function docs() {
+  return (
+    <>
+      <Head>
+        <title>Docs - AgentWebb</title>
+        <meta name="title" content="Docs - AgentWebb" />
+        <meta
+          name="description"
+          content="Get unbiased and honest information about the real estate buying and selling process. Learn from 25+ years of experience and be ready for any red flags."
+        />
+      </Head>
+      <main className="">
+        <div>docs</div>
+      </main>
+    </>
+  );
+}
