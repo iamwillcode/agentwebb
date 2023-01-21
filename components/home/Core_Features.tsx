4abf6e21636688c2a189c9848c00524c8b5b3e54
Justin Bento <bento.a.justin@gmail.com>
2023-01-20 19:16:16 -0500

    updated typography to current typescale

---
+++ components/home/Core_Features.tsx
@@ -0,0 +1,28 @@
+export default function Core_Features() {
+  return (
+    <section className="p-4 mx-auto max-w-7xl sm:px-6 lg:px-8 lg:p-0">
+  <div className="grid items-center grid-cols-1 mb-24 md:grid-cols-2 gap-y-10 md:gap-y-32 gap-x-10 md:gap-x-24">
+    <div>
+      <h3 className="mb-4 headline-medium">Make your dream home a reality</h3>
+      <p className="mb-5 body-large">
+        Handle your subscriptions and transactions efficiently with the clear overview in Dashboard. Features like the smart search option allow you to quickly find any data you’re looking for.
+      </p>
+      <a href="#" className="btn-primary">Learn More</a>
+    </div>
+    <div className="w-full h-full py-48 bg-gray-200"></div>
+  </div>
+  <div className="grid flex-col-reverse items-center grid-cols-1 md:grid-cols-2 gap-y-10 md:gap-y-32 gap-x-10 md:gap-x-24">
+    <div className="order-none md:order-2">
+      <h4 className="mb-4 headline-medium">The most comprehensive real estate resource</h4>
+      <p className="mb-5 body-large">
+        Love to code? Next to our ready-made and free plugins you can use our expansive yet simple API; decide how you integrate Payments and build advanced and reliable products yourself from
+        scratch.
+      </p>
+      <a href="#" className="btn-primary">Learn More</a>
+    </div>
+    <div className="w-full h-full py-48 bg-gray-200"></div>
+  </div>
+</section>
+
+  )
+}
