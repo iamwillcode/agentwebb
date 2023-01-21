b5e480bbcc21caf8cf149f51f97dbe7dd285bac3
Justin Bento <bento.a.justin@gmail.com>
2023-01-21 10:38:47 -0500

    created a page called serveices

---
+++ pages/services.tsx
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+export default function Services() {
+  return (
+    <>
+      <Head>
+        <title>Services - AgentWebb</title>
+        <meta name="title" content="Services - AgentWebb" />
+        <meta
+          name="description"
+          content="Get unbiased and honest information about the real estate buying and selling process. Learn from 25+ years of experience and be ready for any red flags."
+        />
+      </Head>
+      <main className="my-16 space-y-16">
+        <div className="wrapper">Services</div>
+      </main>
+    </>
+  );
+}
