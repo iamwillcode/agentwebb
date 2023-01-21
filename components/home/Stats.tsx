1fe601b6662d9c61f535130238038cf176ee29d3
Justin Bento <bento.a.justin@gmail.com>
2023-01-20 19:16:37 -0500

    updated typography

--- components/home/Stats.tsx
+++ components/home/Stats.tsx
@@ -3,24 +3,26 @@ export default function Example() {
     <div className="bg-blue-800">
       <div className="px-6 py-12 mx-auto max-w-7xl sm:py-16 lg:px-8 lg:py-20">
         <div className="max-w-4xl mx-auto text-center">
-          <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-            Backed by professionals and Right & Home! 
+          <h2 className="display-small text-blue-50">
+          Star power for sale: See whats available now
+
+ 
           </h2>
-          <p className="mt-3 text-xl text-blue-200 sm:mt-4">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus repellat laudantium.
+          <p className="mt-3 text-blue-100 body-large">
+            We get our sources from various professionals and Right & Home realty so our info is modern!.
           </p>
         </div>
         <dl className="mt-10 text-center sm:mx-auto sm:grid sm:max-w-3xl sm:grid-cols-3 sm:gap-8">
           <div className="flex flex-col">
-            <dt className="order-2 mt-2 text-lg font-medium leading-6 text-blue-200">Pepperoni</dt>
+            <dt className="order-2 mt-2 text-gray-200 title-medium">Pepperoni</dt>
             <dd className="order-1 text-5xl font-bold tracking-tight text-white">100%</dd>
           </div>
           <div className="flex flex-col mt-10 sm:mt-0">
-            <dt className="order-2 mt-2 text-lg font-medium leading-6 text-blue-200">Delivery</dt>
+            <dt className="order-2 mt-2 text-gray-200 title-medium">Delivery</dt>
             <dd className="order-1 text-5xl font-bold tracking-tight text-white">24/7</dd>
           </div>
           <div className="flex flex-col mt-10 sm:mt-0">
-            <dt className="order-2 mt-2 text-lg font-medium leading-6 text-blue-200">Viewers</dt>
+            <dt className="order-2 mt-2 text-gray-200 title-medium">Viewers</dt>
             <dd className="order-1 text-5xl font-bold tracking-tight text-white">100k+</dd>
           </div>
         </dl>
