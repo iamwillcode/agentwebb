b8a2eed52f71d695851c55958415ca781a4591b1
Justin Bento <bento.a.justin@gmail.com>
2023-01-23 23:28:52 -0500

    updated layout for both page s

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,180 +1,45 @@
 import Head from "next/head";
-import { HiMail, HiPhone } from "react-icons/hi";
 
-export default function Contact() {
+export default function contact() {
   return (
     <>
       <Head>
         <title>Contact - AgentWebb</title>
-        <meta name="title" content="Contact - AgentWebb" />
-        <meta
-          name="description"
-          content="Get unbiased and honest information about the real estate buying and selling process. Learn from 25+ years of experience and be ready for any red flags."
-        />
       </Head>
-      <main className="">
-        <div className="relative bg-white">
-          <div className="absolute inset-0">
-            <div className="absolute inset-y-0 left-0 w-1/2 bg-gray-50" />
-          </div>
-          <div className="relative mx-auto max-w-7xl lg:grid lg:grid-cols-5">
-            <div className="px-6 py-16 bg-gray-50 lg:col-span-2 lg:px-8 lg:py-24 xl:pr-12">
-              <div className="max-w-lg mx-auto">
-                <h2 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
-                  Get in touch
-                </h2>
-                <p className="mt-3 text-gray-500 body-large">
-                  Nullam risus blandit ac aliquam justo ipsum. Quam mauris
-                  volutpat massa dictumst amet. Sapien tortor lacus arcu.
-                </p>
-                <dl className="mt-8 text-gray-500 body-large">
-                  <div>
-                    <dt className="sr-only">Postal address</dt>
-                    <dd>
-                      <p className="body-large">742 Evergreen Terrace</p>
-                      <p className="body-large">Springfield, OR 12345</p>
-                    </dd>
-                  </div>
-                  <div className="mt-6">
-                    <dt className="sr-only">Phone number</dt>
-                    <dd className="flex">
-                      <HiPhone
-                        className="flex-shrink-0 w-6 h-6 text-gray-400"
-                        aria-hidden="true"
-                      />
-                      <span className="ml-3">+1 (555) 123-4567</span>
-                    </dd>
-                  </div>
-                  <div className="mt-3">
-                    <dt className="sr-only">Email</dt>
-                    <dd className="flex">
-                      <HiMail
-                        className="flex-shrink-0 w-6 h-6 text-gray-400"
-                        aria-hidden="true"
-                      />
-                      <span className="ml-3">support@example.com</span>
-                    </dd>
-                  </div>
-                </dl>
-              </div>
+      <main className="my-24 space-y-24">
+        <section className="wrapper">
+          <div className="grid grid-cols-1 gap-8 lg:grid-cols-2">
+            <div className="">
+              <h1 className="capitalize display-small">Contact Information</h1>
             </div>
-            <div className="px-6 py-16 bg-white lg:col-span-3 lg:py-24 lg:px-8 xl:pl-12">
-              <div className="max-w-lg mx-auto lg:max-w-none">
-                <form
-                  action="#"
-                  method="POST"
-                  className="grid grid-cols-1 gap-y-6"
-                >
-                  <div>
-                    <label htmlFor="full-name" className="sr-only">
-                      Full name
-                    </label>
-                    <input
-                      type="text"
-                      name="full-name"
-                      id="full-name"
-                      autoComplete="name"
-                      className="block w-full px-4 py-3 placeholder-gray-500 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-                      placeholder="Full name"
-                    />
-                  </div>
-                  <div>
-                    <label htmlFor="email" className="sr-only">
-                      Email
-                    </label>
-                    <input
-                      id="email"
-                      name="email"
-                      type="email"
-                      autoComplete="email"
-                      className="block w-full px-4 py-3 placeholder-gray-500 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-                      placeholder="Email"
-                    />
-                  </div>
-                  <div>
-                    <label htmlFor="phone" className="sr-only">
-                      Phone
-                    </label>
-                    <input
-                      type="text"
-                      name="phone"
-                      id="phone"
-                      autoComplete="tel"
-                      className="block w-full px-4 py-3 placeholder-gray-500 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-                      placeholder="Phone"
-                    />
-                  </div>
-                  <div>
-                    <label htmlFor="message" className="sr-only">
-                      Message
-                    </label>
-                    <textarea
-                      id="message"
-                      name="message"
-                      rows={4}
-                      className="block w-full px-4 py-3 placeholder-gray-500 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-                      placeholder="Message"
-                      defaultValue={""}
-                    />
-                  </div>
-                  <div>
-                    <button
-                      type="submit"
-                      className="inline-flex justify-center px-6 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                    >
-                      Submit
-                    </button>
-                  </div>
-                </form>
-              </div>
+            <div className="w-full bg-gray-200 h-[500px] rounded-xl"></div>
+          </div>
+        </section>
+        <section className="wrapper">
+          <h2 className="capitalize display-small">Office Location</h2>
+        </section>
+        <section className="wrapper">
+          <h3 className="capitalize display-small">Office Location</h3>
+        </section>
+        <section className="wrapper">
+          <h4 className="capitalize display-small">Frequently asked questions</h4>
+          <p className="prose">Can&#39;t find the answer you&#39;re looking for? Reach out to our customer support team.</p>
+        </section>
+        <section className="wrapper">
+          <h5 className="capitalize display-small">Office Location</h5>
+          <p className="mt-2 prose">Varius facilisi mauris sed sit. Non sed et duis dui leo, vulputate id malesuada non. Cras aliquet purus dui laoreet diam sed lacus, fames.</p>
+          <div className="">
+            <div className="">
+              <h5 className="headline-small">Toronto</h5>
+              <p className="prose">7363 Cynthia Pass</p>
+              <p className="prose">Toronto, ON N3Y 4H8</p>
             </div>
           </div>
-        </div>
-        <Example />
+        </section>
+        <section className="wrapper">
+          <h6 className="capitalize display-small">Your feedback helps us to improve. Thank you for sharing.</h6>
+        </section>
       </main>
     </>
-  );
-}
-
-function Example() {
-  return (
-    <section className="bg-purple-700">
-      <div className="px-6 py-16 mx-auto max-w-7xl lg:px-8">
-        <div className="max-w-lg mx-auto md:grid md:max-w-none md:grid-cols-2 md:gap-8">
-          <div className="mt-12 sm:mt-16 md:mt-0">
-            <h2 className="text-2xl font-bold text-gray-50 sm:text-3xl sm:tracking-tight">
-              Right at Home Realty
-            </h2>
-            <p className="mt-1.5 body-large text-gray-50">
-              Lorem, ipsum dolor sit amet5 consectetur adipisicing elit. Magni,
-              repellat error corporis doloribus similique, voluptatibus numquam
-              quam, quae officiis facilis.
-            </p>
-            <div className="mt-9">
-              <div className="flex">
-                <div className="flex-shrink-0">
-                  <HiPhone
-                    className="w-6 h-6 text-gray-50"
-                    aria-hidden="true"
-                  />
-                </div>
-                <div className="ml-3 body-medium text-gray-50">
-                  <p>+1 (555) 123 4567</p>
-                  <p className="mt-1">Mon-Fri 8am to 6pm PST</p>
-                </div>
-              </div>
-              <div className="flex mt-6">
-                <div className="flex-shrink-0">
-                  <HiMail className="w-6 h-6 text-gray-50" aria-hidden="true" />
-                </div>
-                <div className="ml-3 body-medium text-gray-50">
-                  <p>support@example.com</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+  )
 }
--- pages/listings.tsx
+++ pages/listings.tsx
@@ -14,8 +14,8 @@ export default function Listings() {
       </Head>
       <main className="my-16 space-y-16">
         <section className="wrapper">
-          <h1 className="font-bold display-small">Listings</h1>
-          <p className="body-large max-w-[90ch] mt-2 text-lg text-gray-600 mb-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus magnam inventore eum accusamus aut dicta! Sequi cupiditate laudantium, nemo, quisquam, libero ut temporibus voluptate eaque laboriosam eligendi perferendis aliquam quas.</p>
+          <h1 className="display-small">Listings</h1>
+          <p className="mb-4 prose">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus magnam inventore eum accusamus aut dicta! Sequi cupiditate laudantium, nemo, quisquam, libero ut temporibus voluptate eaque laboriosam eligendi perferendis aliquam quas.</p>
           <Link href="/listings/slug" className="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Slug Items</Link>
         </section>
       </main>
