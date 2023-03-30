049fc8f1d9a4ae966f751f7efcb9465a49feec31
Justin Bento <bento.a.justin@gmail.com>
2023-03-30 19:06:20 -0400

    removed compnoents

--- components/home/Features.tsx
+++
@@ -1,9 +0,0 @@
-export default function Features() {
-  return (
-    <section className="bg-primary-300">
-    <div className="py-16 wrapper lg:py-24">
-    <h3 className="headline-medium">Properties Sold By Yours Truly</h3>
-    </div>
-  </section>
-  )
-}
