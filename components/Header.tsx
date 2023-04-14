4a0565bbfd807c2ad06eaf661daa47ce0cdf3197
Justin Bento <bento.a.justin@gmail.com>
2023-04-14 00:49:11 -0400

    changed appbar header background colour to gray 100

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,7 +14,7 @@ const navigation = [
 export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
   return (
-    <header className="absolute inset-x-0 top-0 z-50">
+    <header className="absolute inset-x-0 top-0 z-50 bg-gray-100 shadow">
       <nav className="flex items-center justify-between p-6 mx-auto max-w-7xl lg:px-8" aria-label="Global">
         <div className="flex lg:flex-1">
           <Link href="/" className="-m-1.5 p-1.5">
