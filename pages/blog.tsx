23fd9f7d4c0c4e5323ed9521587d18be0ad6d0a7
Justin Bento <bento.a.justin@gmail.com>
2023-04-15 00:21:42 -0400

    fixed error by moving article cardds into their unique component

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -2,39 +2,50 @@ import Blog_Card from "@/components/Blog_Card";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
-
 export const getStaticProps = async () => {
   const res = await fetch("http://localhost:3000/api/blogAPI");
   const data = await res.json();
   return {
-    props: { ninjas: data },
+    props: { articles: data },
   };
 };
 
-export default function Blog({ ninjas }: any) {
+export default function Blog({ articles }: any) {
+  const artcile_cards = articles.map((article: any) => (
+    <Blog_Card
+      key={article.id}
+      title={article.title}
+      description={article.body}
+      buttonLabel="View More"
+      imageUrl={article.media}
+      linkUrl={`/blog/${article.title
+        .toString()
+        .toLowerCase()
+        .split(" ")
+        .join("-")}`}
+    />
+  ));
   return (
     <>
       <Header />
       <main className="container p-4 py-32 mx-auto">
         <section className="max-w-5xl space-y-3">
-          <h1 className="font-bold headline-large">Unlocking the Secrets of the Real Estate</h1>
-          <p className="body-large">With years of experience and a deep understanding of the market, AgentWeeb offers expert advice and a personalized approach to every client. From pre-listing preparations to closing the deal, our team provides transparent communication, unparalleled expertise, and more.</p>
+          <h1 className="font-bold headline-large">
+            Unlocking the Secrets of the Real Estate
+          </h1>
+          <p className="body-large">
+            With years of experience and a deep understanding of the market,
+            AgentWeeb offers expert advice and a personalized approach to every
+            client. From pre-listing preparations to closing the deal, our team
+            provides transparent communication, unparalleled expertise, and
+            more.
+          </p>
         </section>
         <section className="grid grid-cols-1 gap-8 mt-8 lg:grid-cols-3">
-          {ninjas
-            .map((ninja: any) => (
-              <>
-                <Blog_Card title={ninja.title}
-                  description={ninja.body}
-                  buttonLabel="View More"
-                  imageUrl={ninja.media}
-                  linkUrl={`/blog/${ninja.title.toString().toLowerCase().split(' ').join('-')}`} />
-              </>
-            ))}
+          {artcile_cards}
         </section>
       </main>
       <Footer />
-
     </>
   );
 }
