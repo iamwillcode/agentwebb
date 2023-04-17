6766edc67d37da869b559b9bd28389f18f7084a7
Justin Bento <bento.a.justin@gmail.com>
2023-04-17 19:40:13 -0400

    fixed breadcrumb error

--- components/Breadcrumb.tsx
+++ components/Breadcrumb.tsx
@@ -1,32 +1,33 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import clsx from "clsx";
 
 export default function Breadcrumb() {
   const router = useRouter();
   const pathnames = router.pathname.split("/").filter((x) => x);
   return (
-    <>
-      <nav aria-label="breadcrumb">
-        <ol className="flex items-center mb-4 space-x-3 capitalize body-medium">
-          <li className="breadcrumb-item">
-            <Link href="/">Home</Link>
-          </li>
-          {pathnames.map((name, index) => {
-            const routeTo = `/${pathnames.slice(0, index + 1).join("/")}`;
-            const isLast = index === pathnames.length - 1;
-            return (
-              <>
-                <li className={`breadcrumb-item ${isLast ? "active" : ""}`}
-                  aria-current={isLast ? "page" : ""}
-                  key={routeTo}
-                >
-                  {isLast ? name : <Link href={routeTo}>{name}</Link>}
-                </li>
-              </>
-            );
-          })}
-        </ol>
-      </nav>
-    </>
+    <nav aria-label="breadcrumb">
+      <ol className="flex items-center mb-4 space-x-3 capitalize body-medium">
+        <li className="breadcrumb-item">
+          <Link href="/">Home</Link>
+        </li>
+        {pathnames.map((name, index) => {
+          const routeTo = `/${pathnames.slice(0, index + 1).join("/")}`;
+          const isLast = index === pathnames.length - 1;
+          return (
+            <li
+              key={routeTo}
+              className={clsx(
+                "breadcrumb-item",
+                isLast && "active"
+              )}
+              aria-current={isLast ? "page" : undefined}
+            >
+              {isLast ? name : <Link href={routeTo}>{name}</Link>}
+            </li>
+          );
+        })}
+      </ol>
+    </nav>
   );
 }
